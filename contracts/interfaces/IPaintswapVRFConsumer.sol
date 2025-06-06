7ff94c2e34cd69cfad77fa7f99f9636f645fa468
doublesharp <jsilver@doublesharp.com>
2025-06-05 17:02:26 -0700

    The VRF

--- contracts/interfaces/IPaintswapVRFConsumer.sol
+++ contracts/interfaces/IPaintswapVRFConsumer.sol
@@ -6,7 +6,7 @@ interface IPaintswapVRFConsumer {
      * @notice rawFulfillRandomWords handles the VRF response and your contract must implement it!
      *
      * @param requestId The Id initially returned by requestRandomness
-     * @param randomWords the VRF output expanded to the requested number of words
+     * @param randomWords The VRF output expanded to the requested number of words
      */
     function rawFulfillRandomWords(
         uint256 requestId,
